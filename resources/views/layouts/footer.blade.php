848c4ddaf70caec8bb9c04b4ce5e83deb39735a5
ArpitX <arpit.bansal2010@gmail.com>
2017-04-26 19:21:56 +0530

    share links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,6 +3,7 @@
 <div class="text">
 &copy <a href="#">Common drive</a>
 </div>
+
 <div class="icons">
     <a href=""><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
     <a href=""><i class="fa fa-google-plus-official fa-2x" aria-hidden="true"></i></a>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -52,6 +52,6 @@
             $('.drawer-space').toggleClass('open');
     }
     </script>
-
+    <!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59009e9045159ccf"></script> 
   </body>
 </html>
