d84fd1becd015396abf81fe49a1def99e75bb1e0
ArpitX <arpit.bansal2010@gmail.com>
2017-04-28 17:50:18 +0530

    icons

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,12 +4,14 @@
 &copy <a href="#">Common drive</a>
 </div>
 
+
 <div class="icons">
-    <a href=""><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+    <a href="https://twitter.com/arpitbansalx" target="_blank"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
     <a href=""><i class="fa fa-google-plus-official fa-2x" aria-hidden="true"></i></a>
     <a href=""><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-    <a href=""><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
-    <!-- <a href=""><i class="fa fa-pinterest fa-2x" aria-hidden="true"></i></a> -->
+    <!-- <a href=""><i class="fa fa-github fa-2x" aria-hidden="true"></i></a> -->
+    <a href=""><i class="fa fa-pinterest fa-2x" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-dribbble fa-2x" aria-hidden="true"></i></a>
     <!-- <a href=""><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> -->
   </div>
 
